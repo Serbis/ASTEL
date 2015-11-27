a961f2900c5da6c631a6809b63ccffb6dc2562ac
Serbis <OVVYALKIN@gmail.com>
2015-11-27 14:21:03 +0300

    -

--- app/src/main/java/solkris/ru/aste/MainActivity.java
+++ app/src/main/java/solkris/ru/aste/MainActivity.java
@@ -43,24 +43,74 @@ public class MainActivity extends ActionBarActivity {
 
         String textinhtml = "<font color=\"#FF8C00\" size=\"4\" face=\"Arial, Helvetica, sans-serif\"><u><i><b>class</font><br>" +
                 "<font color=\"#00FF00\" size=\"4\" face=\"Courier New, Courier, monospace\"><u><i><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;class</font>";
+
+        //Create an array of descriptions of keywords
         List<Keyword> keywords = new ArrayList<Keyword>();
+        keywords.add(new Keyword("abstract", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("assert", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("boolean", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("break", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("byte", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("case", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("catch", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("char", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
         keywords.add(new Keyword("class", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
-        keywords.add(new Keyword("public", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
-        keywords.add(new Keyword("static", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
-        keywords.add(new Keyword("void", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("const", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("continue", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("default", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("do", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("double", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("else", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("enum", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("extends", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("final", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("finally", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("float", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("for", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("if", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("implements", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("import", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("instanceof", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
         keywords.add(new Keyword("int", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
-        keywords.add(new Keyword("String", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("interface", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("long", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("native", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("new", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("package", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("private", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("protected", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("public", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("return", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("try", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("static", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("strictfp", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("super", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("switch", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("synchronized", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("this", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("throw", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("throws", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("transient", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("try", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("void", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("volatile", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
+        keywords.add(new Keyword("while", new FontStyle(1, "FF8C00", "Arial", false, false, false)));
         keywords.add(new Keyword("\"", new FontStyle(1, "00FF00", "Arial", false, false, false)));
+        keywords.add(new Keyword("'", new FontStyle(1, "00FF00", "Arial", false, false, false)));
+
+        //Styling standard tokens
         FontStyle numbersStyle = new FontStyle(1, "0000FF", "Arial", false, true, false);
         FontStyle constStyle = new FontStyle(1, "FF0000", "Arial", false, false, false);
         FontStyle textStyle = new FontStyle(1, "000000", "Arial", false, true, false);
 
+        //Assign by styles
         SyntaxEditText syntaxEditText = (SyntaxEditText) findViewById(R.id.set1);
         syntaxEditText.setKeywordList(keywords);
         syntaxEditText.setNumbersStyle(numbersStyle);
         syntaxEditText.setConstantsStyle(constStyle);
         syntaxEditText.setTextStyle(textStyle);
         syntaxEditText.setText(text);
+
 
         //syntaxEditText.getEditableText().replace(3, 15, Html.fromHtml("<font color=\"#FF00FF\" size=\"4\" face=\"Arial, Helvetica, sans-serif\"><u><i><b>ХУЙ</font>"));
 
--- app/src/main/java/solkris/ru/aste/lexer/Tag.java
+++
@@ -1,9 +0,0 @@
-package solkris.ru.aste.lexer;
-
-/**
- * It describes the numeric codes tokens
- */
-public class Tag {
-    /** An array of token codes */
-    public static final int KEY = 256, NUM = 257, SPACE = 259, TEXT = 260;
-}
